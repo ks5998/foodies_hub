b28c13be3a55c895c8f8a5dfa63fe5b28fdacd01
Kundan Singh <kundasin591998@outlook.com>
2022-08-05 11:17:11 +0530

    checkpoint 2

--- src/COMPONENTS/Navbar.jsx
+++ src/COMPONENTS/Navbar.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react"
+
+
 export default function Navbar(){
+
+    const [showSidebar, setShowSidebar] = useState(false);
+
     return(
         <div className="navbar container">
             <a href="#" className="logo">F<span>oo</span>diesHub</a>
@@ -6,6 +12,13 @@ export default function Navbar(){
                 <a href="#" className="active">Home</a>
                 <a href="#">Recipes</a>
                 <a href="#">Settings</a>
+            </div>
+            <div 
+                className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+                onClick={() => setShowSidebar(!showSidebar)}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
             </div>
         </div>
     )
