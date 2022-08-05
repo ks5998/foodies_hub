1568332ec7f63c3daf3103a4e0cb3da45cf60318
Kundan Singh <kundasin591998@outlook.com>
2022-08-05 12:48:38 +0530

    checkpoint 3

--- src/COMPONENTS/Navbar.jsx
+++ src/COMPONENTS/Navbar.jsx
@@ -1,17 +1,44 @@
 import { useState } from "react"
+import Sidebar from "./Sidebar";
+import { faHome, faList, faCog } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Navbar(){
 
     const [showSidebar, setShowSidebar] = useState(false);
 
+    const links = [
+        {
+            name: "Home",
+            path: "/",
+            icon: faHome
+        },
+        {
+            name: "Recipes",
+            path: "/recipes",
+            icon: faList
+        },
+        {
+            name: "Settings",
+            path: "/settings",
+            icon: faCog
+        }
+    ]
+
+    function closeSidebar(){
+        setShowSidebar(false);
+    }
+
     return(
+        <>
         <div className="navbar container">
             <a href="#" className="logo">F<span>oo</span>diesHub</a>
             <div className="nav-links">
-                <a href="#" className="active">Home</a>
-                <a href="#">Recipes</a>
-                <a href="#">Settings</a>
+                {
+                    links.map((link) => (
+                        <a href="#" key={link.name}>{link.name}</a>
+                    ))
+                }
             </div>
             <div 
                 className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
@@ -21,5 +48,9 @@ export default function Navbar(){
                 <div className="bar"></div>
             </div>
         </div>
+        { showSidebar && <Sidebar close={ closeSidebar } links={ links } /> } 
+        </>
+
+        
     )
 }
